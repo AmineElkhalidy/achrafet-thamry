0be79d5df843bc1663c7f3fe1531c44bcd600a49
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-08 21:24:15 +0100

    Fixing header

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -6,6 +6,14 @@ const Header = () => {
   useEffect(() => {
     showMenu();
     removeMenu();
+
+    function scrollHeader() {
+      const header = document.getElementById("header");
+      // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+      if (this.scrollY >= 50) header.classList.add("scroll-header");
+      else header.classList.remove("scroll-header");
+    }
+    window.addEventListener("scroll", scrollHeader);
   }, []);
 
   const showMenu = () => {
@@ -36,15 +44,6 @@ const Header = () => {
     }
     navLink.forEach((n) => n.addEventListener("click", linkAction));
   };
-
-  //TODO: You should change that later
-  // const scrollUp = () => {
-  //   const scrollUp = document.getElementById("scroll-up");
-  //   // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  //   if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
-  //   else scrollUp.classList.remove("show-scroll");
-  // };
-  // window.addEventListener("scroll", scrollUp);
 
   return (
     <header className="header" id="header">
